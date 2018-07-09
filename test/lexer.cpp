67f523cf1578ff88e3472b1ae5f567101509a790
crnbrdrck <broderickciaran@gmail.com>
2018-07-09 11:02:46 +0100

    Changed getNextToken test to use the example1 sapphire script

--- test/lexer.cpp
+++ test/lexer.cpp
@@ -1,25 +1,72 @@
 #include<iostream>
+#include<fstream>
 #include<string>
 #include "../src/lexer/lexer.h"
 using namespace std;
 
 int testNextToken() {
     // Test the getNextToken method of the lexer class
-    cout << "ensure lexer.getNextToken returns the correct sequence of tokens for a given input" << endl;
-    string input = "=+(){},:";
-    cout << "input: '" << input << "'" << endl;
+    cout << "ensure lexer.getNextToken returns the correct sequence of tokens for `examples/example1.sph`" << endl;
+    ifstream file("examples/example1.sph");
+    // TODO - Just pass the file directly as input to the lexer
+    string input((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
+    cout << input << endl;
 
     // Build an array of expected tokens
     Token expectedTokens[] = {
-        Token(TokenType::ASSIGN, "="),
-        Token(TokenType::PLUS, "+"),
-        Token(TokenType::LPAREN, "("),
-        Token(TokenType::RPAREN, ")"),
-        Token(TokenType::LBRACE, "{"),
-        Token(TokenType::RBRACE, "}"),
-        Token(TokenType::COMMA, ","),
+        // let five: int = 5
+        Token(TokenType::LET, "let"),
+        Token(TokenType::IDENT, "five"),
         Token(TokenType::COLON, ":"),
-        Token(TokenType::END_OF_FILE, ""),
+        Token(TokenType::TYPE, "int"),
+        Token(TokenType::ASSIGN, "="),
+        Token(TokenType::INTEGER, "5"),
+
+        // let ten: int = 10
+        Token(TokenType::LET, "let"),
+        Token(TokenType::IDENT, "ten"),
+        Token(TokenType::COLON, ":"),
+        Token(TokenType::TYPE, "int"),
+        Token(TokenType::ASSIGN, "="),
+        Token(TokenType::INTEGER, "10"),
+
+        // def add(x: num, y: num) -> num {
+        Token(TokenType::DEFINE, "def"),
+        Token(TokenType::IDENT, "add"),
+        Token(TokenType::LPAREN, "("),
+        Token(TokenType::IDENT, "x"),
+        Token(TokenType::COLON, ":"),
+        Token(TokenType::TYPE, "num"),
+        Token(TokenType::COMMA, ","),
+        Token(TokenType::IDENT, "y"),
+        Token(TokenType::COLON, ":"),
+        Token(TokenType::TYPE, "num"),
+        Token(TokenType::RPAREN, ")"),
+        Token(TokenType::RETURN_TYPE, "->"),
+        Token(TokenType::TYPE, "num"),
+        Token(TokenType::LBRACE, "{"),
+
+        //     return x + y
+        Token(TokenType::RETURN, "return"),
+        Token(TokenType::IDENT, "x"),
+        Token(TokenType::PLUS, "+"),
+        Token(TokenType::IDENT, "y"),
+
+        // }
+        Token(TokenType::RBRACE, "}"),
+
+        // let result: num = add(five, ten)
+        Token(TokenType::LET, "let"),
+        Token(TokenType::IDENT, "result"),
+        Token(TokenType::COLON, ":"),
+        Token(TokenType::TYPE, "num"),
+        Token(TokenType::ASSIGN, "="),
+        Token(TokenType::IDENT, "add"),
+        Token(TokenType::LPAREN, "("),
+        Token(TokenType::IDENT, "five"),
+        Token(TokenType::COMMA, ","),
+        Token(TokenType::IDENT, "ten"),
+        Token(TokenType::RPAREN, ")"),
     };
 
     // Create a lexer based on the input
