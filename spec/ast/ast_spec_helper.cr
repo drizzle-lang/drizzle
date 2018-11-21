9c80e624832abe31e855b5303558a4f26a29478f
crnbrdrck <broderickciaran@gmail.com>
2018-11-21 14:48:56 +0000

    Moved check parser errors to spec_helper

--- spec/ast/ast_spec_helper.cr
+++ spec/ast/ast_spec_helper.cr
@@ -1,21 +1,5 @@
 require "../spec_helper"
 
-# Helper function that ensures there are no parser errors.
-#
-# If there are, it will print them out
-def check_parser_errors(parser : Drizzle::Parser)
-  errors = parser.errors
-  if errors.empty?
-    return
-  end
-
-  errors.each do |err|
-    puts err
-  end
-
-  errors.size.should eq 0
-end
-
 # Helper function to test if an expression is an IntegerLiteral and has the expected value
 def test_integer_literal(exp : Drizzle::AST::Expression?, expected : Int64)
   int = exp.as(Drizzle::AST::IntegerLiteral)
