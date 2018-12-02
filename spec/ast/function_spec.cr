21a46a400d4a168ce36f0a5a589ff8971697d344
crnbrdrck <broderickciaran@gmail.com>
2018-12-02 18:53:40 +0000

    Made a start on parsing functions
    Need to figure out exactly what it is I want to do regarding datatypes
    for identifiers

---
+++ spec/ast/function_spec.cr
@@ -0,0 +1,7 @@
+require "./ast_spec_helper"
+require "../spec_helper"
+
+describe Drizzle::AST::Function do
+  it "properly generates the correct node structure for the function described in `examples/example5.drzl`" do
+  end
+end
---
+++ src/drizzle/ast/function.cr
@@ -0,0 +1,50 @@
+require "./block"
+require "./identifier"
+require "./statement"
+require "../token"
+
+module Drizzle
+  module AST
+    # Node representing a function definition. Functions are first-class in Drizzle, meaning that function names can be used as normal expressions.
+    #
+    # The difference between what Drizzle does and what Monkey does is that in Monkey, the function literals themselves are expressions, whereas in Drizzle they are statements.
+    class Function < Statement
+      @token : Token
+      @name : Identifier
+      @params : Array(Identifier)
+      @ret_type : Identifier
+      @body : BlockStatement
+
+      def initialize(@token : Token, @name : Identifier, @params : Array(Identifier), @ret_type : Identifier, @body : BlockStatement)
+      end
+
+      def literal : String
+        return @token.literal
+      end
+
+      def to_s : String
+        param_strings = [] of String
+        @params.each do |param|
+          param_strings << param.to_s
+        end
+
+        return "#{@token.literal} #{@name.to_s} (#{param_strings.join ", "}) #{@body.to_s}"
+      end
+
+      # The token that led to the creation of the node
+      getter token
+
+      # The Identifier node representing the name of the function
+      getter name
+
+      # An array of Identifiers representing parameters to the function
+      getter params
+
+      # The return type of the function
+      getter ret_type
+
+      # The block making up the body of the function
+      getter body
+    end
+  end
+end
