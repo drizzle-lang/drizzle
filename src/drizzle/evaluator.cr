3fe1f7dceb55cef7f1ebad3845c1f454fc2af7b4
freyamade <frybrdrck@gmail.com>
2019-02-24 18:03:37 +0000

    Turns out I'm going to need the catch all for a while

--- src/drizzle/evaluator.cr
+++ src/drizzle/evaluator.cr
@@ -5,12 +5,18 @@ module Drizzle
   # Module containing an overloaded evaluation function that evaluates all the various node types
   class Evaluator
     # eval method for program nodes, the starting point of any drizzle program
-    # def self.eval(node : AST::Program) : Object::Object
-    # end
+    def self.eval(node : AST::Program) : Object::Object
+      return Evaluator.eval node.statements[0]
+    end
 
     # eval method for integer literal nodes
     def self.eval(node : AST::IntegerLiteral) : Object::Object
       return Object::Integer.new node.value
+    end
+
+    # temp catchall method
+    def self.eval(node : AST::Node) : Object::Object
+      return Object::Null.new
     end
   end
 end
